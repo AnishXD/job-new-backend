8295ec50818af6149131557b154cff95a62301ca
AnishXD <110338891+AnishXD@users.noreply.github.com>
2024-09-05 19:02:25 +0530

    Update user.controller.js done check

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -80,23 +80,23 @@ export const login = async (req, res) => {
 
         const tokenData = {
             userId: user._id
-        }
-        const token = await jwt.sign(tokenData, process.env.SECRET_KEY, { expiresIn: '1d' });
-
-        user = {
+        };
+        
+        const token = await jwt.sign(tokenData, process.env.JWT_SECRET_KEY, { expiresIn: '1d' });
+        
+        return res.status(200).cookie("token", token, { 
+            maxAge: 1 * 24 * 60 * 60 * 1000, // 1 day
+            httpOnly: false,                 // Set to true if you don't need to access the cookie via JavaScript
+            secure: true,         // ensures the cookie is only sent over HTTPS
+            sameSite: 'None',                // Set to 'None' and secure=true for cross-site requests
+            path: '/'                        // Makes the cookie accessible across the entire site
+        }).json({
             _id: user._id,
-            fullname: user.fullname,
-            email: user.email,
-            phoneNumber: user.phoneNumber,
-            role: user.role,
-            profile: user.profile
-        }
+            username: user.username,
+            fullName: user.fullName,
+            profilePhoto: user.profilePhoto
+        });
 
-        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpsOnly: true, sameSite: 'lax' }).json({
-            message: `Welcome back ${user.fullname}`,
-            user,
-            success: true
-        })
     } catch (error) {
         console.log(error);
     }
